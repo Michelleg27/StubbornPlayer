23211270cc5f9c29fc86975863cf4c5a62f19080
michelleg27 <mgomez48@mail.csuchico.edu>
2018-03-01 11:43:50 -0800

    Finished

--- main.cpp
+++ main.cpp
@@ -6,6 +6,25 @@ using namespace std;
 int main()
 {
   SecretDoor game;
-    
+  int response; 
+  int won;
+  
+
+  cout<<"How many times would you want to play?"<<endl; 
+  cin>>response;
+
+  for (int i = 0; i < response; i++)
+  {
+    game.newGame(); 
+  
+    game.guessDoorC(); 
+    game.guessDoorC();  
+   
+     if( game.isWinner() == true)
+     {
+         won++;
+     }
+  }
+    cout<<"You've won "<<won<<" times!"<<endl;
   return 0;
 }
